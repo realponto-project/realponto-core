9c6ebd71750322b4b15e1df6f845ff7b028e2379
jessiLeandro <jessi_leandro@hotmail.com>
2021-03-09 10:58:46 -0300

    change type birthday column

--- database/models/user.model.js
+++ database/models/user.model.js
@@ -1,57 +1,74 @@
 const Sequelize = require('sequelize')
+const { compare, hash } = require('bcrypt')
 const uuidv4Generator = require('../../utils/helpers/hash')
 
 const User = (sequelize) => {
-  const User = sequelize.define('user', {
-    id: {
-      allowNull: false,
-      type: Sequelize.STRING,
-      primaryKey: true,
-      defaultValue: uuidv4Generator('us_')
+  const User = sequelize.define(
+    'user',
+    {
+      id: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        primaryKey: true,
+        defaultValue: uuidv4Generator('us_')
+      },
+      activated: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: ''
+      },
+      badget: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      birthday: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      document: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: ''
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      firstAccess: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+      }
     },
-    activated: {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
-      defaultValue: true
-    },
-    name: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      unique: true
-    },
-    phone: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      defaultValue: ''
-    },
-    badget: {
-      type: Sequelize.STRING,
-      allowNull: true,
-    },
-    birthday: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      defaultValue: ''
-    },
-    document: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      defaultValue: ''
-    },
-    password: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    firstAccess: {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
-      defaultValue: true
+
+    {
+      hooks: {
+        beforeSave: async (user) => {
+          if (user.password) {
+            user.password = await hash(user.password, 10)
+          }
+        }
+      }
     }
-  })
+  )
+
+  User.prototype.checkPassword = async function (password) {
+    return compare(password, this.password)
+  }
 
   User.associate = (models) => {
     models.user.belongsTo(models.company, {
