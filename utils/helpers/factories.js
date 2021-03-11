b84f5ba75295252d6bc9a401b258053aa8b98093
jessiLeandro <jessi_leandro@hotmail.com>
2021-03-11 17:23:13 -0300

    chrore: create factory user

--- utils/helpers/factories.js
+++ utils/helpers/factories.js
@@ -1,11 +1,21 @@
 const { factory } = require('factory-girl')
+
 const database = require('../../database')
-const AddressModel = database.model('address')
-const CustomerModel = database.model('customer')
 const { generatorFakerAddress } = require('./Faker/address')
 const { generatorFakerCustomer } = require('./Faker/customer')
+const { generatorFakerUser } = require('./Faker/user')
+
+const AddressModel = database.model('address')
+const CustomerModel = database.model('customer')
+const UserModel = database.model('user')
+
+factory.define('user', UserModel, {
+  ...generatorFakerUser(),
+  companyId: 'co_4095e6c0-056d-4b6d-b857-a35584634ad0'
+})
 
 factory.define('address', AddressModel, generatorFakerAddress())
+
 factory.define('customer', CustomerModel, {
   ...generatorFakerCustomer(),
   addressId: factory.assoc('address', 'id'),
