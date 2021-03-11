4e3015eae3eeb7d764a2e6c258ead4f4692d9d87
Guilherme Stain <guilhermestain@localhost.localdomain>
2021-03-11 17:25:50 -0300

    chore: factory test

--- utils/helpers/factories.js
+++ utils/helpers/factories.js
@@ -34,7 +34,6 @@ factory.define('status', StatusModel, {
   ...fakerStatus(),
   companyId: 'co_4095e6c0-056d-4b6d-b857-a35584634ad0'
 })
-
 factory.define('product', ProductModel, {
   ...fakerProduct(),
   companyId: 'co_4095e6c0-056d-4b6d-b857-a35584634ad0'
