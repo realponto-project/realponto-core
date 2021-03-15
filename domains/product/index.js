0645d32ffc9977d55308765815a0a175477b1143
Guilherme Stain <guilhermestain@localhost.localdomain>
2021-03-15 14:11:54 -0300

    chore: add initial_balance on product

--- domains/product/index.js
+++ domains/product/index.js
@@ -3,14 +3,46 @@ const database = require('../../database')
 const productSchema = require('../../utils/helpers/Schemas/product')
 
 const ProductModel = database.model('product')
+const TransactionModel = database.model('transaction')
+const OrderModel = database.model('order')
+const StatusModel = database.model('status')
 
 const buildSearchAndPagination = buildPagination('product')
+const statusCreate = 'initial_balance'
 
 class ProductDomain {
   async create(bodyData, options = {}) {
     const { transaction = null } = options
     await productSchema.validate(bodyData, { abortEarly: false })
-    return await ProductModel.create(bodyData, { transaction })
+    const productCreated = await ProductModel.create(bodyData, { transaction })
+    const statusFinded = await StatusModel.findOne({
+      where: {
+        companyId: bodyData.companyId,
+        label: statusCreate
+      }
+    })
+    const orderCreated = await OrderModel.create(
+      {
+        companyId: bodyData.companyId,
+        statusId: statusFinded.id,
+        userId: bodyData.userId
+      },
+      { transaction }
+    )
+
+    await TransactionModel.create(
+      {
+        orderId: orderCreated.id,
+        statusId: statusFinded.id,
+        quantity: productCreated.balance,
+        productId: productCreated.id,
+        userId: bodyData.userId,
+        companyId: bodyData.companyId
+      },
+      { transaction }
+    )
+
+    return productCreated
   }
 
   async update(id, bodyData, options = {}) {
@@ -18,8 +50,10 @@ class ProductDomain {
     await productSchema.validate(bodyData, { abortEarly: false })
 
     const searchProduct = await ProductModel.findByPk(id)
+    await searchProduct.update(bodyData, { transaction })
+    await searchProduct.reload()
 
-    return await searchProduct.update(bodyData, { transaction })
+    return searchProduct
   }
 
   async getById(id, companyId) {
