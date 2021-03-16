15ac395530d3c8df65b4be3f32dd406e0f2c7885
Guilherme Stain <guilhermestain@localhost.localdomain>
2021-03-16 14:44:40 -0300

    chore: controller company

--- routes/company/index.js
+++ routes/company/index.js
@@ -2,5 +2,7 @@ const router = require('express').Router()
 const { companyController } = require('../../controllers')
 
 router.get('/companies/:id', companyController.getById)
+router.get('/users', companyController.getAll)
+router.put('/companies/:id', companyController.update)
 
 module.exports = router
