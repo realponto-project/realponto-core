9295004055c07293a9b75832d5a63a776ebb12e9
Guilherme Stain <guilhermestain@localhost.localdomain>
2021-03-16 14:44:40 -0300

    chore: controller company

--- controllers/company/index.test.js
+++ controllers/company/index.test.js
@@ -2,6 +2,8 @@ const request = require('supertest')
 const app = require('../../index')
 const faker = require('faker')
 const truncate = require('../../utils/truncate')
+const factory = require('../../utils/helpers/factories')
+const { fakerCompany } = require('../../utils/helpers/fakers')
 
 const companyMock = {
   company: {
@@ -29,21 +31,93 @@ describe('company controller', () => {
       const res = await request(app).post('/register').send(companyMock)
       expect(res.statusCode).toBe(201)
       expect(res.request.method).toStrictEqual('POST')
+      expect(companyMock.company).toHaveProperty('name')
+      expect(companyMock.company).toHaveProperty('fullname')
+      expect(companyMock.company).toHaveProperty('document')
+      expect(companyMock.company).toHaveProperty('siteUrl')
+      expect(companyMock.company).toHaveProperty('allowOrder')
+      expect(companyMock.company).toHaveProperty('allowPdv')
+      expect(companyMock.user).toHaveProperty('name')
+      expect(companyMock.user).toHaveProperty('email')
+      expect(companyMock.user).toHaveProperty('password')
+      expect(companyMock.user).toHaveProperty('birthday')
     })
   })
 
-  // describe('get company by id', function (id) {
-  // 	afterAll(async () => await truncate())
+  describe('update company controller', () => {
+    let token = null
+    let companyFactory = null
 
-  //   it('responds with json', function (done) {
-  // 		expect.hasAssertions()
+    beforeAll(async () => {
+      const response = await request(app).post('/auth/login').send({
+        email: 'alexandre_santos@hotmail.com',
+        password: '123456'
+      })
 
-  //     request(app)
-  //       .get(`/api/companies/${id}`)
-  //       .auth('username', 'password')
-  //       .set('Accept', 'application/json')
-  //       .expect('Content-Type', /json/)
-  //       .expect(200, done)
-  //   })
-  // })
+      token = response.body.token
+
+      companyFactory = await factory.create('company')
+    })
+
+    it('update company', () => {
+      const companiesMock = fakerCompany()
+
+      const res = request(app)
+        .put(`/api/companies/${companyFactory.id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send(companiesMock)
+      expect(res.method).toStrictEqual('PUT')
+      expect(companiesMock).toHaveProperty('name')
+      expect(companiesMock).toHaveProperty('fullname')
+      expect(companiesMock).toHaveProperty('document')
+      expect(companiesMock).toHaveProperty('siteUrl')
+      expect(companiesMock).toHaveProperty('allowOrder')
+      expect(companiesMock).toHaveProperty('allowPdv')
+    })
+  })
+
+  describe('get company controller', () => {
+    let token = null
+    let companyFactory = null
+
+    beforeAll(async () => {
+      companyFactory = await factory.create('company')
+      const response = await request(app).post('/auth/login').send({
+        email: 'alexandre_santos@hotmail.com',
+        password: '123456'
+      })
+
+      token = response.body.token
+    })
+
+    it('get company by id', () => {
+      const res = request(app)
+        .get(`/api/companies/${companyFactory.id}`)
+        .set('Authorization', `Bearer ${token}`)
+      expect(res.method).toStrictEqual('GET')
+    })
+  })
+
+  describe('get company controller', () => {
+    let token = null
+    let companyFactory = null
+
+    beforeAll(async () => {
+      companyFactory = await factory.create('company')
+
+      const response = await request(app).post('/auth/login').send({
+        email: 'alexandre_santos@hotmail.com',
+        password: '123456'
+      })
+
+      token = response.body.token
+    })
+
+    it('get all company', () => {
+      const res = request(app)
+        .get('/api/companies')
+        .set('Authorization', `Bearer ${token}`)
+      expect(res.method).toStrictEqual('GET')
+    })
+  })
 })
