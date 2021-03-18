a5c72476ae87a0aed6092ba6b4fa279fc13a9dd0
Guilherme Stain <guilhermestain@localhost.localdomain>
2021-03-18 17:43:17 -0300

    review: conflicts

--- routes/company/index.js
+++ routes/company/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const { companyController } = require('../../controllers')
 
 router.get('/companies/:id', companyController.getById)
-router.get('/users', companyController.getAll)
+router.get('/companies', companyController.getAll)
 router.put('/companies/:id', companyController.update)
 
 module.exports = router
