be5fb693d94c0a670b9b7e78ab49992261ac7b5d
jessiLeandro <jessi_leandro@hotmail.com>
2021-03-18 10:00:53 -0300

    fix factories

--- utils/helpers/factories.js
+++ utils/helpers/factories.js
@@ -16,47 +16,43 @@ const UserModel = database.model('user')
 const OrderModel = database.model('order')
 const TransactionModel = database.model('transaction')
 
-factory.define('user', UserModel, {
+factory.define('user', UserModel, () => ({
   ...generatorFakerUser(),
   companyId: 'co_4095e6c0-056d-4b6d-b857-a35584634ad0'
-})
+}))
 
-factory.define('address', AddressModel, generatorFakerAddress())
+factory.define('address', AddressModel, () => generatorFakerAddress())
 
-factory.define('customer', CustomerModel, {
+factory.define('customer', CustomerModel, () => ({
   ...generatorFakerCustomer(),
   addressId: factory.assoc('address', 'id'),
   companyId: 'co_4095e6c0-056d-4b6d-b857-a35584634ad0'
-})
+}))
 
-factory.define('company', CompanyModel, fakerCompany())
+factory.define('company', CompanyModel, () => fakerCompany())
 
-factory.define('status', StatusModel, {
+factory.define('status', StatusModel, () => ({
   ...fakerStatus(),
   companyId: 'co_4095e6c0-056d-4b6d-b857-a35584634ad0'
-})
+}))
 
-factory.define('product', ProductModel, {
+factory.define('product', ProductModel, () => ({
   ...fakerProduct(),
   companyId: 'co_4095e6c0-056d-4b6d-b857-a35584634ad0'
-})
+}))
 
-factory.define('transaction', TransactionModel, {
+factory.define('transaction', TransactionModel, () => ({
   quantity: 10,
   productId: factory.assoc('product', 'id'),
   companyId: 'co_4095e6c0-056d-4b6d-b857-a35584634ad0'
-})
+}))
 
-factory.define(
-  'order',
-  OrderModel,
-  (fakeTransaction = faker.random.boolean()) => ({
-    pendingReview: fakeTransaction,
-    statusId: factory.assoc('status', 'id', { fakeTransaction }),
-    customerId: factory.assoc('customer', 'id'),
-    userId: factory.assoc('user', 'id'),
-    companyId: 'co_4095e6c0-056d-4b6d-b857-a35584634ad0'
-  })
-)
+factory.define('order', OrderModel, () => ({
+  pendingReview: false,
+  statusId: factory.assoc('status', 'id', { fakeTransaction: false }),
+  customerId: factory.assoc('customer', 'id'),
+  userId: factory.assoc('user', 'id'),
+  companyId: 'co_4095e6c0-056d-4b6d-b857-a35584634ad0'
+}))
 
 module.exports = factory
