6f5eb02806e4888abe5fe118f1fac603dcb62abe
Guilherme Stain <guilhermestain@localhost.localdomain>
2021-03-18 10:28:35 -0300

    chore: controller status

--- utils/helpers/factories.js
+++ utils/helpers/factories.js
@@ -25,6 +25,7 @@ factory.define('address', AddressModel, () => generatorFakerAddress())
 factory.define('customer', CustomerModel, () => ({
   ...generatorFakerCustomer(),
   addressId: factory.assoc('address', 'id'),
+
   companyId: 'co_4095e6c0-056d-4b6d-b857-a35584634ad0'
 }))
 
