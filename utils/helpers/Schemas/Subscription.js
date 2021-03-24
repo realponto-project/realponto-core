af7deec1829527ddc7cb92dea17e9db9c06a5612
Guilherme Stain <guilhermestain@localhost.localdomain>
2021-03-24 17:38:30 -0300

    teste

--- utils/helpers/Schemas/Subscription.js
+++ utils/helpers/Schemas/Subscription.js
@@ -1,15 +1,15 @@
 const yup = require('yup')
 
 const subscriptionSchema = yup.object().shape({
-  activated: yup.boolean().required(),
-  startDate: yup.string().required(),
-  endDate: yup.string().required(),
-  autoRenew: yup.boolean().required(),
-  paymentMethod: yup
-    .string()
-    .matches(/(credit_card|boleto|cash)/)
-    .required(),
-  statusPayment: yup.string().required(),
+  // activated: yup.boolean().required(),
+  // startDate: yup.string().required(),
+  // endDate: yup.string().required(),
+  // autoRenew: yup.boolean().required(),
+  // paymentMethod: yup
+  //   .string()
+  //   .matches(/(credit_card|boleto|cash)/)
+  //   .required(),
+  // statusPayment: yup.string().required(),
   planId: yup.string().required()
 })
 
