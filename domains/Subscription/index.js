88a9712518305871e21588a8d1b87156875f08ab
Guilherme Stain <guilhermestain@localhost.localdomain>
2021-03-24 17:38:30 -0300

    teste

--- domains/Subscription/index.js
+++ domains/Subscription/index.js
@@ -1,6 +1,8 @@
 const database = require('../../database')
 const SubscricptionModel = database.model('subscription')
+const PlanModel = database.model('plan')
 const SubscriptionSchema = require('../../utils/helpers/Schemas/Subscription')
+const moment = require('moment')
 
 class SubscriptionDomain {
   async create(bodyData, options = {}) {
@@ -8,7 +10,18 @@ class SubscriptionDomain {
 
     await SubscriptionSchema.validate(bodyData, { abortEarly: false })
 
-    return SubscricptionModel.create(bodyData, { transaction })
+    const searchPlanActivated = await PlanModel.findOne({
+      where: { id: bodyData.planId, activated: true }
+    })
+
+    if (searchPlanActivated === null) {
+      throw new Error('Erro')
+    }
+
+    return SubscricptionModel.create(
+      { ...bodyData, endDate: moment().add(13, 'months').format('L') },
+      { transaction }
+    )
   }
 }
 
