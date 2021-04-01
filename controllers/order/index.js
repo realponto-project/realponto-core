93fb1b1cab171c25b622342f6c420ffd9b172133
jessiLeandro <jessi_leandro@hotmail.com>
2021-04-01 12:14:56 -0300

    chore: add columns on table order

--- controllers/order/index.js
+++ controllers/order/index.js
@@ -2,6 +2,9 @@ const { pathOr } = require('ramda')
 
 const database = require('../../database')
 const OrderDomain = require('../../domains/Order')
+const PdvSchema = require('../../utils/helpers/Schemas/Pdv')
+
+const StatusModel = database.model('status')
 
 const create = async (req, res, next) => {
   const transaction = await database.transaction()
@@ -10,6 +13,39 @@ const create = async (req, res, next) => {
   try {
     const response = await OrderDomain.create(
       { ...req.body, companyId },
+      { transaction }
+    )
+
+    await transaction.commit()
+    res.status(201).json(response)
+  } catch (error) {
+    await transaction.rollback()
+    res.status(400).json({ error: error.message })
+  }
+}
+
+const createPdv = async (req, res, next) => {
+  const transaction = await database.transaction()
+  const companyId = pathOr(null, ['decoded', 'user', 'companyId'], req)
+
+  try {
+    const statusPdv = await StatusModel.findOne({
+      where: {
+        companyId,
+        activated: true,
+        label: 'sale',
+        value: 'Venda'
+      },
+      attributes: ['id'],
+      raw: true
+    })
+
+    if (!statusPdv) res.status(500).json({ message: 'Status not found' })
+
+    await PdvSchema.validate(req.body)
+
+    const response = await OrderDomain.create(
+      { ...req.body, companyId, statusId: statusPdv.id },
       { transaction }
     )
 
@@ -47,5 +83,6 @@ const getAll = async (req, res, next) => {
 module.exports = {
   create,
   getById,
-  getAll
+  getAll,
+  createPdv
 }
