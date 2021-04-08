e3768a777077e550bb25c37b569e6eb5963b6999
jessiLeandro <jessi_leandro@hotmail.com>
2021-04-08 09:29:44 -0300

    chore: add valitator on customer document

--- database/models/customer.model.js
+++ database/models/customer.model.js
@@ -1,5 +1,8 @@
 const Sequelize = require('sequelize')
 const uuidv4Generator = require('../../utils/helpers/hash')
+const { isValid: isValidCpf } = require('@fnando/cpf')
+const { isValid: isValidCnpj } = require('@fnando/cnpj')
+const { isEmpty, replace } = require('ramda')
 
 const Customer = (sequelize) => {
   const Customer = sequelize.define('customer', {
@@ -19,7 +22,17 @@ const Customer = (sequelize) => {
     },
     document: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: true,
+      set(value) {
+        this.setDataValue('document', replace(/\D/g, '', value))
+      },
+      validate: {
+        isCpfOrCnpj(value) {
+          if (!isEmpty(value) && !isValidCnpj(value) && !isValidCpf(value)) {
+            throw new Error('Value is not cpnj nor cpf')
+          }
+        }
+      }
     },
     phone: {
       type: Sequelize.STRING,
