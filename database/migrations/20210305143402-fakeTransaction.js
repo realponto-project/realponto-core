4bfc7bc974ded78b7ed0210138576d628ab59425
jessiLeandro <jessi_leandro@hotmail.com>
2021-04-15 09:20:17 -0300

    Fix Migration

--- database/migrations/20210305143402-fakeTransaction.js
+++ database/migrations/20210305143402-fakeTransaction.js
@@ -42,7 +42,7 @@ module.exports = {
       orderId: {
         type: Sequelize.STRING,
         references: {
-          model: 'orders',
+          model: 'order',
           key: 'id'
         },
         onUpdate: 'cascade',
