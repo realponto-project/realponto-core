ea1044cf0c43add316a0db9142f6ed6cc7f55159
jessiLeandro <jessi_leandro@hotmail.com>
2021-07-05 13:47:48 -0300

    Add queueping server

--- services/queue/queues.js
+++ services/queue/queues.js
@@ -6,10 +6,12 @@ const updateAdsOnDBQueue = new Queue('update ads on database', redisConfig)
 const adsQueue = new Queue('loader ads', redisConfig)
 const refreshTokenQueue = new Queue('refresh token', redisConfig)
 const reprocessQueue = new Queue('reprocess ads mercado libre', redisConfig)
+const pingServerQueue = new Queue('ping server', redisConfig)
 
 module.exports = {
   adsQueue,
   refreshTokenQueue,
   updateAdsOnDBQueue,
-  reprocessQueue
+  reprocessQueue,
+  pingServerQueue
 }
