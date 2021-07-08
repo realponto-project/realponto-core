96706d1fdd0b8241d5ed53c6239ec7811e0ff9e5
jessiLeandro <jessi_leandro@hotmail.com>
2021-07-08 14:41:00 -0300

    chore: remove loarderAds

--- routes/ML/index.js
+++ routes/ML/index.js
@@ -7,7 +7,6 @@ router.post('/ml-accounts', MlController.createAccount)
 router.get('/ml-accounts', MlController.getAllAccounts)
 router.get('/ml-accounts/:id', MlController.getAccount)
 router.get('/ml-ads', MlController.getAllAds)
-router.get('/ml-load-ads/:mlAccountId', MlController.loadAds)
 
 router.post('/ml-update-ads/:mlAccountId', MlController.updateAdsByAccount)
 
