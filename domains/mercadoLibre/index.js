66c2f6b7941d5b5bca36fc0c9e7f1433522321e0
Guilherme Stain <guilhermestain@localhost.localdomain>
2023-06-26 14:42:27 -0300

    add: change factor

--- domains/mercadoLibre/index.js
+++ domains/mercadoLibre/index.js
@@ -151,7 +151,7 @@ class MercadoLibreDomain {
               price: pipe(
                 prop('costPrice'),
                 multiply(1.65),
-                // add(adBuilded.shippingCost || 6),
+                // add(adBuilded.shippingCost || 6),,
                 ifElse(lte(43.50), add(adBuilded.shippingCost || 7), add(7)),
                 (value) => value.toFixed(2),
                 Number,
